68bc155784c6d1b0d3bca96f24d9037d49002dd2
YouJeen-ru <1.rouch@mail.ru>
2021-05-13 13:29:27 +0300

    Slider with js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper-container', {
+
+    loop: true,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
