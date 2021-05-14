57c5f5c2f607021303bb2c8cfa2439a6f582e4ae
YouJeen-ru <1.rouch@mail.ru>
2021-05-14 15:34:38 +0300

    universal fn smooth scrolling and open/close modal realized

--- js/main.js
+++ js/main.js
@@ -13,3 +13,38 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-scrollbar',
     },
 });
+
+// cart
+
+const buttonCart = document.querySelector('.button-cart')
+const modalCart = document.querySelector('#modal-cart')
+const modalClose = document.querySelector('.modal-close')
+
+const openModal = function () {
+    modalCart.classList.add('show')
+}
+
+const closeModal = function () {
+    modalCart.classList.remove('show')
+}
+
+buttonCart.addEventListener('click', openModal)
+modalClose.addEventListener('click', closeModal)
+
+
+// universal fn smooth scrolling
+
+ {
+    const scrollLinks = document.querySelectorAll('a.scroll-link')
+
+    for (let i = 0; i < scrollLinks.length; i++) {
+        scrollLinks[i].addEventListener('click', function (event) {
+            event.preventDefault()
+            const id = scrollLinks[i].getAttribute('href')
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
+}
