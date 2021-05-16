310208eee852227c3e1fec2a6c51b4ac77e6f278
YouJeen-ru <1.rouch@mail.ru>
2021-05-16 22:04:02 +0300

    min ref universal fn smooth scrolling

--- js/main.js
+++ js/main.js
@@ -44,10 +44,10 @@ modalCart.addEventListener('click', function (event) {
  {
     const scrollLinks = document.querySelectorAll('a.scroll-link')
 
-    for (let i = 0; i < scrollLinks.length; i++) {
-        scrollLinks[i].addEventListener('click', function (event) {
+    for (const scrollLink of scrollLinks) {
+        scrollLink.addEventListener('click', function (event) {
             event.preventDefault()
-            const id = scrollLinks[i].getAttribute('href')
+            const id = scrollLink.getAttribute('href')
             document.querySelector(id).scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
