198b918aa650a5ac82e743b14901239057c85bda
YouJeen-ru <1.rouch@mail.ru>
2021-05-16 22:01:33 +0300

    view all goods with fetch fake dataBase

--- js/main.js
+++ js/main.js
@@ -55,3 +55,54 @@ modalCart.addEventListener('click', function (event) {
         })
     }
 }
+
+// goods
+
+const more = document.querySelector('.more')
+const navigationItem = document.querySelectorAll('.navigation-item')
+const longGoodsList = document.querySelector('.long-goods-list')
+
+const getGoods = async function () {
+    const result = await fetch('db/db.json')
+    if (!result.ok) {
+        throw 'Error' + result.status
+    }
+    return result.json()
+}
+
+getGoods().then(function (data) {
+    console.log(data)
+})
+
+const createCard = function (objCard) {
+    const card = document.createElement('div')
+    card.className = 'col-lg-3 col-sm-6'
+    card.innerHTML = `
+    
+    <div class="goods-card">
+    ${objCard.label ? `<span class="label">${objCard.label}</span>` : '' }
+                    
+                    <img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
+                    <h3 class="goods-title">${objCard.name}</h3>
+                    <p class="goods-description">${objCard.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
+                        <span class="button-price">${objCard.price}</span>
+                    </button>
+                </div>
+    `
+
+    return card
+}
+
+
+const renderCards = function (data) {
+    longGoodsList.textContent = ''
+    const cards = data.map(createCard)
+    longGoodsList.append(...cards)
+    document.body.classList.add('show-goods')
+}
+
+more.addEventListener('click', function (event) {
+    event.preventDefault()
+    getGoods().then(renderCards)
+})
