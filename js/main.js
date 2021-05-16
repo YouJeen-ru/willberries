56f83cb26b70566406da8c5024749c3aff7f72a5
YouJeen-ru <1.rouch@mail.ru>
2021-05-16 22:26:24 +0300

    filterCards function  realized

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ modalCart.addEventListener('click', function (event) {
 // goods
 
 const more = document.querySelector('.more')
-const navigationItem = document.querySelectorAll('.navigation-item')
+const navigationLink = document.querySelectorAll('.navigation-link')
 const longGoodsList = document.querySelector('.long-goods-list')
 
 const getGoods = async function () {
@@ -74,19 +74,20 @@ getGoods().then(function (data) {
     console.log(data)
 })
 
-const createCard = function (objCard) {
+const createCard = function ({ label, name, img, description, id , price }) {
     const card = document.createElement('div')
     card.className = 'col-lg-3 col-sm-6'
+
     card.innerHTML = `
-    
+   
     <div class="goods-card">
-    ${objCard.label ? `<span class="label">${objCard.label}</span>` : '' }
+    ${label ? `<span class="label">${label}</span>` : '' }
                     
-                    <img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
-                    <h3 class="goods-title">${objCard.name}</h3>
-                    <p class="goods-description">${objCard.description}</p>
-                    <button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
-                        <span class="button-price">${objCard.price}</span>
+                    <img src="db/${img}" alt="${name}" class="goods-image">
+                    <h3 class="goods-title">${name}</h3>
+                    <p class="goods-description">${description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${id}">
+                        <span class="button-price">${price}</span>
                     </button>
                 </div>
     `
@@ -105,4 +106,24 @@ const renderCards = function (data) {
 more.addEventListener('click', function (event) {
     event.preventDefault()
     getGoods().then(renderCards)
+})
+
+const filterCards = function (field, value) {
+    getGoods()
+        .then(function (data) {
+        const filteredGoods = data.filter(function (good) {
+            return good[field] === value
+        })
+        return filteredGoods
+    })
+        .then(renderCards)
+}
+
+navigationLink.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        event.preventDefault()
+        const field = link.dataset.field
+        const value = link.textContent
+        filterCards(field, value)
+    })
 })
